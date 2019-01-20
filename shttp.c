b1b2cea53755e2e91361ac173ce15ffc61e8adbe
Chenxiao <645400928@qq.com>
2019-01-21 01:51:08 +0800

    http request handler

--- shttp.c
+++ shttp.c
@@ -1,2 +1,21 @@
 #include "dbg.h"
+#include "shttp.h"
 
+//get http request line
+int get_request_line(int sock, char *buf, int line_len) {
+	int cnt = 0;
+
+	return cnt;
+error: 
+	return -1;
+}
+
+//NOT CGI version
+int handler(int sock) {
+	debug("Hello, I am client socket handler.");
+
+	return 0;
+error:
+	log_err("Client handler error.");
+	return -1;
+}
